e06b4f8360ffbd3f2d2ca79c5a811d853d9a48ba
AntonTevseev <91051811+AntonTevseev@users.noreply.github.com>
2021-12-19 03:04:58 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write(" Hello");
+            Write(" World");
+            Write("!!!");
+            ReadKey();
+        }
+    }
+}
